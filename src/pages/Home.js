4be98c631e2206246a185102b1847e90f8297d2c
Kolya Mazurok <kolya.mazurok2@gmail.com>
2022-06-30 17:10:01 +0300

    Resolve 5

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
-import { CssBaseline, Typography } from '@mui/material';
+import { CssBaseline, Pagination, Typography } from '@mui/material';
 import { Container } from '@mui/system';
-import { useCallback, useState } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import TodoCounter from '../components/todo/TodoCounter';
 import TodoTable from '../components/todo/TodoTable';
@@ -11,9 +11,10 @@ const Home = () => {
 
   const { entities: todos, isLoading, isError } = useSelector((state) => state.todos);
 
-  const [pageState] = useState({
+  const [pageState, setPageState] = useState({
     page: 1,
-    perPage: 10
+    perPage: 10,
+    totalPages: 1
   });
 
   const handleChange = (id, isCompleted) => {
@@ -29,11 +30,19 @@ const Home = () => {
     });
   };
 
+  const handlePageChange = (event, page) => {
+    setPageState({ ...pageState, page: page });
+  };
+
   const getFilteredTodos = useCallback(() => {
     return [...todos].slice(
       (pageState.page - 1) * pageState.perPage,
       pageState.perPage * pageState.page
     );
+  }, [todos, pageState]);
+
+  const countTotalPages = useMemo(() => {
+    return Math.ceil(todos.length / pageState.perPage);
   }, [todos, pageState]);
 
   const filteredTodos = getFilteredTodos();
@@ -56,6 +65,8 @@ const Home = () => {
       {!isLoading && !isError && <TodoCounter todos={filteredTodos} />}
 
       {!isLoading && !isError && <TodoTable todos={filteredTodos} onChange={handleChange} />}
+
+      <Pagination count={countTotalPages} color="primary" onChange={handlePageChange} />
     </Container>
   );
 };
